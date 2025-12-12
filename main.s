ac6f1226850fa73b8c304ddbe15049fea4c1e4bd
SILAS-JOB <tiilas777@gmail.com>
2025-12-12 12:53:26 -0300

    Project Start

---
+++ main.s
@@ -0,0 +1,11 @@
+	.file	"main.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	movl	$67, %eax
+	ret
+	.size	main, .-main
+	.ident	"GCC: (GNU) 15.2.1 20251111 (Red Hat 15.2.1-4)"
+	.section	.note.GNU-stack,"",@progbits
+
